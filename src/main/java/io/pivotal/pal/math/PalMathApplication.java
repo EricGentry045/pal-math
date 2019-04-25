ab8996ad7cadedaf8a34c65f6a7a0c549f48426b
pal_user <eric.gentry045@myci.csuci.edu>
2019-04-25 16:56:29 -0700

    Starting the pal-math application

---
+++ src/main/java/io/pivotal/pal/math/PalMathApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.pal.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalMathApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(PalMathApplication.class, args);
+    }
+}
